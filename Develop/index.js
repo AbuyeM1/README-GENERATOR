1aa02505998cc42c97cb84c308f668b0b4248002
Abuye Mamuye <abuyeye3@gmail.com>
2021-01-17 12:37:39 -0800

    modified the index.js and add package-lock.json

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,30 @@
 // TODO: Include packages needed for this application
+ const inquirer = require('inquirer');
+ const fs = require('fs');
+ const util = require('util');
+
+ const api = require('./utils/api.js');
+ const generateMarkdown = require('./util/generateMarkdown');
+
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+    type: "input",
+    message: "What is GitHub username? AbuyeM1",
+    name: "username",
+    Validate: function(answer){
+        if (answer.length < 1){
+            return console.log("A valid GitHub username is required");
+        } return true;
+
+    }
+    },
+    {
+        type: 'input',
+        message: 'What is project name?'
+    }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
