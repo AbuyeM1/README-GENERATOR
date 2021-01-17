edbfd93fd6427a5a8e586cc798ec6fab8cbba958
Abuye Mamuye <abuyeye3@gmail.com>
2021-01-17 14:07:37 -0800

    add code on index.js

--- Develop/index.js
+++ Develop/index.js
@@ -2,28 +2,60 @@
  const inquirer = require('inquirer');
  const fs = require('fs');
  const util = require('util');
-
- const api = require('./utils/api.js');
- const generateMarkdown = require('./util/generateMarkdown');
+//  console.log('hello');
+//  const api = require('./utils/api.js');
+//  const generateMarkdown = require('./util/generateMarkdown');
 
 
 // TODO: Create an array of questions for user input
 const questions = [
     {
     type: "input",
-    message: "What is GitHub username? AbuyeM1",
+    message: "What is your GitHub username?",
     name: "username",
+    default: 'AbuyeM1',
     Validate: function(answer){
         if (answer.length < 1){
             return console.log("A valid GitHub username is required");
         } return true;
-
+    
     }
     },
     {
         type: 'input',
-        message: 'What is project name?'
+        message: 'What is your email address?',
+        name: 'email address',
+        default: 'abuye20@yahoo.com',
+        Validate: function(answer){
+            if (answer.length < 1){
+                return console.log("A valid email address is required");
+            } return true;
     }
+},
+    {
+        type: 'input',
+        message: 'What is your project name',
+        name: 'name',
+        default: 'COVID-19 Tracker',
+        Validate: function(answer){
+            if (answer.length < 1){
+                return console.log("A valid project name required");
+            } return true;
+
+
+    }
+},
+{
+    type:'input',
+    message:'Write a description of your project',
+    name:'description',
+    default:'show covid-19 case data',
+    validdate: function(answer) {
+        if (answer.length < 1){
+            return console.log("A this.valid project decscription is require.")
+        }
+    }
+}
 ];
 
 // TODO: Create a function to write README file
