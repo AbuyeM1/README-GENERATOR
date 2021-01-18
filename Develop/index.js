072c8f8e78ad9d1a2dc29db0528a22bc50dcd261
Abuye Mamuye <abuyeye3@gmail.com>
2021-01-17 21:43:10 -0800

    create the object for all

--- Develop/index.js
+++ Develop/index.js
@@ -2,11 +2,9 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 const util = require('util');
-//  console.log('hello');
-// const api = require('./utils/api.js');
-const generateMarkdown = require('./utils/generateMarkdown.js');
-const { type } = require('os');
 
+//  console.log('hello');
+const generateMarkdown = require('./utils/generateMarkdown.js');
 // console.log("hi");
 // TODO: Create an array of questions for user input
 const questions = [
@@ -19,7 +17,6 @@ const questions = [
             if (answer.length < 1) {
                 return console.log("A valid GitHub username is required");
             } return true;
-
         }
     },
     {
@@ -35,16 +32,14 @@ const questions = [
     },
     {
         type: 'input',
-        message: 'What is your project name',
+        message: 'What is your project name?',
         name: 'name',
         default: 'COVID-19 Tracker',
         Validate: function (answer) {
             if (answer.length < 1) {
                 return console.log("A valid project name required");
-            } 
+            }
             return true;
-
-
         }
     },
     {
@@ -55,34 +50,76 @@ const questions = [
         validdate: function (answer) {
             if (answer.length < 1) {
                 return console.log("A this.valid project decscription is require.")
-           }
-           return true;
+            }
+            return true;
         }
     },
     {
         type: 'list',
         message: 'A lincense for your project.',
-        choices: 'MIT License',
+        choices: ['MIT License','Boost Software License 1.0'],
         name: 'License',
     },
-    {   
-        type:'input',
-        message:'What command should be run to install dependencies',
-        name:'installation',
-        default:'Javascript',
+    {
+        type: 'input',
+        message: 'What command should be run to install dependencies?',
+        name: 'installation',
+        default: 'npm iinstall'
+    },
+    {
+        type: 'input',
+        message: 'What command should be run to run tests?',
+        name: 'test',
+        default: 'NA',
+    },
+    {
+        type: 'input',
+        message: 'What does the user need to know about using the repo?',
+        name: 'repo',
+        default: 'Javascript'
+
 
     },
     {
-        type:'input'
+        type: 'input',
+        message: 'What does the user need to know about contributing to the repo?',
+        name: 'contributing',
+        default: 'Javascript'
     }
-
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) { }
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, err => {
+        if (err) {
+            return console.log(err);
+        }
+        console.log("Success your README.md file has been generated")
+    });
+}
+const writeFileAsync = util.promisify(fs.writeFile);
 
 // TODO: Create a function to initialize app
-function init() { }
+async function init() {
+    try {
+        const userResponses = await inquirer.prompt(questions);
+        console.log("your responses:", usrerResponses);
+        console.log("thank you for your responses! fair yoour GitHub data next..");
+
+        const userInfo = await api.getUser(userResponses);
+        console.log("Your GitHub user info: ", userInfo);
+
+        const markdown = generateMarkdown(usrerResponses, userInfo);
+        console.log(markdown);
+
+        await writeFileAsync('README.md', markdown);
+    } catch (error) {
+        console.log(error);
+    }
+
+
+};
 
 // Function call to initialize app
 init();
+
